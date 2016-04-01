ac004a2d0dbe21ca752d8d1ca40097661fdef009
GraemeB <grae22@gmail.com>
2016-04-01 19:40:39 +0200

    Initial commit.

---
+++ EvoTest/Assets/BodySegment.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets
+{
+  public abstract class BodySegment
+  {
+    //---------------------------------------------------------------------------
+
+    protected abstract void GetAppendagePoints(
+      float fillFactor,               // (0.0, 1.0) 0.0 = No appendages; 1.0 = Max # of appendages.
+      out List<Vector3> points );   // Points list to be populated.
+
+    //---------------------------------------------------------------------------
+  }
+}
